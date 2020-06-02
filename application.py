7d7cf3cea2069432ecdb1c9ba34d74c02c4e0820
avidadearthur <arthurtquintao@gmail.com>
2020-06-02 18:57:07 +0000

    Fix @app.route with correct file route

--- application.py
+++ application.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/services.html")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
